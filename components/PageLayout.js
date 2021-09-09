5be9729fb203facf1940ce30a8abb4c8afbcb29e
MichaelBaynon <baynonmichael@gmail.com>
2021-09-09 19:20:05 -0400

    a

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,7 +1,3 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import CustomNavbar from './CustomNavbar.js'
 
 const PageLayout = ({ children }) => {
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,60 @@
 import Head from "next/head";
 import { Container, Row, Card, Button } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel'
+import Link from 'next/link'
+import withRouter from "next/dist/client/with-router";
+
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>ReactJS with react-bootstrap</title>
-        <link rel="icon" href="/favicon-32x32.png" />
+        <title>Albion HQ</title>
+        <link rel='icon' href="/AlbionHQlogo.png" />
       </Head>
-      home page
+      
+      <Carousel>
+        
+        
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/BUILDS.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <Link href='/builds'>Builds</Link>
+      <p>Resources for what build to run for all the content in Albion Online.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/gathering.jpg"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Gathering</h3>
+      <p>Info on how best to gather.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/crafting.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Crafting</h3>
+      <p>Learn how to craft to make millions!</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
     </div>
   );
 }
