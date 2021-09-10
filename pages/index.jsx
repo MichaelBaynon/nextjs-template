6eaecb92511bbd7d699d78f1b539015b3cef0d6b
MichaelBaynon <baynonmichael>
2021-09-10 17:34:07 -0400

    a

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,60 +1,79 @@
 import Head from "next/head";
 import { Container, Row, Card, Button } from "react-bootstrap";
-import Carousel from 'react-bootstrap/Carousel'
-import Link from 'next/link'
+import Carousel from "react-bootstrap/Carousel";
+import Link from "next/link";
 import withRouter from "next/dist/client/with-router";
-
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Albion HQ</title>
-        <link rel='icon' href="/AlbionHQlogo.png" />
+        <link rel="icon" href="/AlbionHQlogo.png" />
       </Head>
-      
-      <Carousel>
-        
-        
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/BUILDS.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <Link href='/builds'>Builds</Link>
-      <p>Resources for what build to run for all the content in Albion Online.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
 
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/gathering.jpg"
-      alt="Second slide"
-    />
+      <Card style={{border: 'none'}} className="bg-dark text-white">
+        <Card.Img src="/BUILDS.jpg" alt="Card image" />
+        <Card.ImgOverlay>
+          <Card.Title  className='homeCardText'>Card title</Card.Title>
+          <Card.Text className='homeCardText'>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+          <Card.Text  className='homeCardText'>Last updated 3 mins ago</Card.Text>
+        </Card.ImgOverlay>
+      </Card>
 
-    <Carousel.Caption>
-      <h3>Gathering</h3>
-      <p>Info on how best to gather.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
+      <div className="homeCards">
+        <Card style={{ width: "18rem" }} className="singeHomeCard">
+          <Card.Img variant="top" src="/BUILDS.jpg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">
+              {" "}
+              <Link href="/builds"> Go somewhere </Link>{" "}
+            </Button>
+          </Card.Body>
+        </Card>
 
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/crafting.jpg"
-      alt="Third slide"
-    />
+        {/* card 2 */}
 
-    <Carousel.Caption>
-      <h3>Crafting</h3>
-      <p>Learn how to craft to make millions!</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+        <Card style={{ width: "18rem" }} className="singeHomeCard">
+          <Card.Img variant="top" src="/BUILDS.jpg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">
+              {" "}
+              <Link href="/builds"> Go somewhere </Link>{" "}
+            </Button>
+          </Card.Body>
+        </Card>
 
+        {/* card 3 */}
+
+        <Card style={{ width: "18rem" }} className="singeHomeCard">
+          <Card.Img variant="top" src="/BUILDS.jpg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">
+              {" "}
+              <Link href="/builds"> Go somewhere </Link>{" "}
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
